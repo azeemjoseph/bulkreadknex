b37306d53f3d02e33aeba3cf935265fc9d63fd62
Azeem Joseph <Azeem.joseph@gmail.com>
2024-07-04 21:17:58 +0500

    api updated

--- routes/data.js
+++ routes/data.js
@@ -2,8 +2,8 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 const xlsx = require("xlsx");
-const knexConfig = require("../knexfile"); // Adjust the path based on your project structure
-const knex = require("knex")(knexConfig.development); // Adjust the environment as per your configuration
+const knexConfig = require("../knexfile"); // knex config file
+const knex = require("knex")(knexConfig.development); // knex object
 
 // Endpoint to read Excel and insert data into MySQL
 router.post("/upload", async (req, res) => {
@@ -19,7 +19,7 @@ router.post("/upload", async (req, res) => {
     const chunkSize = 100; // Number of records to insert per chunk
     for (let i = 0; i < xlData.length; i += chunkSize) {
       const chunk = xlData.slice(i, i + chunkSize);
-      await knex.batchInsert("data", chunk); // Replace 'data' with your actual table name
+      await knex.batchInsert("data", chunk); // batch insert with knex object ( note objection js not wokring with mysql)
     }
 
     res.status(200).send("Data inserted successfully!");
